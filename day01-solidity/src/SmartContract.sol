444e3cee2ead08ef3f22694a8bf0f666694920c6
leoballoch <leo.balloch@epitech.eu>
2026-02-02 12:02:04 +0100

    end(task0.04):fin de la task)

--- day01-solidity/src/SmartContract.sol
+++ day01-solidity/src/SmartContract.sol
@@ -2,5 +2,37 @@
 pragma solidity ^0.8.20;
 
 contract SmartContract {
-    
+    // Public Variables : 
+    uint256 public halfAnswerOfLife = 21;
+    address public myEthereumContractAddress = address(this);
+    address public myEthereumAddress = msg.sender;
+    string public poCIsWhat = "PoC is good, PoC is life.";
+    bytes32 public whoIsTheBest;
+    mapping(string => uint256) public myGrades;
+    string[5] public myPhoneNumber;
+
+    // Internal variable :
+    bool internal _areYouABadPerson = false;
+
+    // Private Variable : 
+    int256 private _youAreACheater = -42;
+
+
+enum roleEnum { STUDENT, TEACHER }
+
+struct Information {
+    string firstname;
+    string lastName;
+    uint8 age;
+    string city;
+    roleEnum role;   
+}
+
+Information public myInformations = Information({
+    firstname: "PoC",
+    lastName: "PoCIs",
+    age: 25,
+    city: "Paris",
+    role : roleEnum.STUDENT
+});
 }
